67d48826e6e00c07c6bc5ca06ce826ac27cab3ee
racinmat <racinmat@fel.cvut.cz>
2017-11-13 01:14:18 +0100

    Unified compression.

--- managed/GTAVisionUtils/ImageUtils.cs
+++ managed/GTAVisionUtils/ImageUtils.cs
@@ -61,7 +61,7 @@ namespace GTAVisionUtils {
                 t.SetField(TiffTag.BITSPERSAMPLE, 8);
                 t.SetField(TiffTag.SUBFILETYPE, FileType.PAGE);
                 t.SetField(TiffTag.PHOTOMETRIC, Photometric.RGB);
-                t.SetField(TiffTag.COMPRESSION, Compression.JPEG);
+                t.SetField(TiffTag.COMPRESSION, Compression.LZW);
                 t.SetField(TiffTag.JPEGQUALITY, 60);
                 t.SetField(TiffTag.PREDICTOR, Predictor.HORIZONTAL);
                 t.SetField(TiffTag.SAMPLEFORMAT, SampleFormat.UINT);
